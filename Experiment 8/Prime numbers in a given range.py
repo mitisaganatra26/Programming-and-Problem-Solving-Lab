e15694e3f5ac216ce38795bbe2f5b9aa814705f0
mitisaganatra26 <mitisaganatra@gmail.com>
2026-05-22 22:35:36 +0530

    Update Prime numbers in a given range.py

--- Experiment 8/Prime numbers in a given range.py
+++ Experiment 8/Prime numbers in a given range.py
@@ -1,29 +1,18 @@
-# 1. Print 'dimension: ' and read n
-print("dimension: ", end="")
-n = int(input())
+start = int(input())
+end = int(input())
 
-# 2. Print 'first matrix:' and read the matrix
-print("first matrix:")
-matrix1 = []
-for _ in range(n):
-    row = list(map(int, input().split()))
-    matrix1.append(row)
-
-# 3. Print 'second matrix:' and read the matrix
-print("second matrix:")
-matrix2 = []
-for _ in range(n):
-    row = list(map(int, input().split()))
-    matrix2.append(row)
-
-# 4. Multiplication Logic (This part of your code was correct!)
-result = [[0 for _ in range(n)] for _ in range(n)]
-for i in range(n):
-    for j in range(n):
-        for k in range(n):
-            result[i][j] += matrix1[i][k] * matrix2[k][j]
-
-# 5. Print Result
-print("Resultant Matrix:")
-for row in result:
-    print(*(row))
+primes = []
+for num in range(start, end+1):
+	if num > 1:
+		is_prime = True
+		for i in range(2, int(num**0.5)+1):
+			if num% i == 0:
+				is_prime = False
+				break
+		if is_prime:
+			primes.append(num)
+if primes:
+	for p in primes:
+		print(p)
+else:
+	print("No primes")
